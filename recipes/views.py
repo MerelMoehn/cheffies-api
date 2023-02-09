0fcd4365d00e740ab1b1f905b5ae39015138d7e7
MerelMoehn <merelmoehn@outlook.com>
2023-02-09 08:43:17 +0000

    Create and delete recipedetail

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,8 @@
 from rest_framework import status, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import Http404
+from cheffies_api.permissions import IsOwnerOrReadOnly
 from .serializers import RecipeSerializer
 from .models import Recipe
 
@@ -18,7 +20,7 @@ class RecipeList(APIView):
             recipes, many=True, context={'request': request}
         )
         return Response(serializer.data)
- 
+
     def post(self, request):
         serializer = RecipeSerializer(
             data=request.data, context={'request': request}
@@ -30,4 +32,37 @@ class RecipeList(APIView):
             )
         return Response(
             serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class RecipeDetail(APIView):
+    serializer_class = RecipeSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def get_object(self, pk):
+        try:
+            recipe = Recipe.objects.get(pk=pk)
+            self.check_object_permissions(self.request, recipe)
+            return recipe
+        except Recipe.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        recipe = self.get_object(pk)
+        serializer = RecipeSerializer(recipe, context={'request': request})
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        recipe = self.get_object(pk)
+        serializer = RecipeSerializer(recipe, data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        recipe = self.get_object(pk)
+        recipe.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
         )
