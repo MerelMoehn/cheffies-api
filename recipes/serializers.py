a9521569ce251d10bd2f9936ae0e3c46d184431c
MerelMoehn <merelmoehn@outlook.com>
2023-02-09 08:14:21 +0000

    Add image validation

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -8,6 +8,21 @@ class RecipeSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 * 3:
+            raise serializers.ValidationError(
+                'The image size is larger than 3MB!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width is larger than 4096px.'
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height is larger than 4096px.'
+            )
+        return value
+
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
